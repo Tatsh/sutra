dd8e8f64dce8704338beb244e45cdbf487577ed4
Tatsh <ddrtist@gmail.com>
2012-01-18 15:55:11 -0600

    Add site root files

---
+++ site/global.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @copyright Copyright (c) 2011 Poluza.
+ * @author Andrew Udvare [au] <andrew@poluza.com>
+ * @license http://www.opensource.org/licenses/mit-license.php
+ *
+ * @package SutraSite
+ * @link http://www.example.com/
+ *
+ * @version 1.0
+ */
+require 'flourish/fLoader.php';
+require 'sutra/sLoader.php';
+require 'moor/Moor.php';
+
+if (is_readable('./router-overrides.inc')) {
+  require './router-overrides.inc';
+}
+
+// Initialise the includes and headers.
+sLoader::best();
+
+// NOTE Does not get modified upstream
+if (is_readable('./includes.inc')) {
+  require './includes.inc';
+}
+
+/**
+ * Get translated version of text if necessary.
+ *
+ * @todo Implement translations as necessary.
+ *
+ * @param string $text Text to search for.
+ * @param array|string,... $vars Replacement key => value pairs or multiple
+ *   string arguments.
+ * @return string The text.
+ *
+ * @package SutraSite
+ */
+function __($text, $vars = array()) {
+  if (!is_array($vars)) {
+    $vars = func_get_args();
+    array_shift($vars);
+  }
+
+  return strtr($text, $vars);
+}
+
+/**
+ * Get a link, possibly a path alias.
+ *
+ * This is an alias for sRouter::linkTo().
+ *
+ * @param string $content Content within the a tag.
+ * @param string $path Non-aliased path to link to.
+ * @param array Array of attributes for the a tag.
+ * @return string HTML tag.
+ *
+ * @package SutraSite
+ *
+ * @see sRouter::linkTo()
+ */
+function l($content, $path, array $attr = array()) {
+  return sRouter::linkTo($path, $content, $attr);
+}
---
+++ site/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Initialisation.
+ *
+ * If not in production mode, sCore::debugCallback() is registered to
+ *   be used by fCore::debug().
+ *
+ * @copyright Copyright (c) 2011 Poluza.
+ * @author Andrew Udvare [au] <andrew@poluza.com>
+ * @license http://www.opensource.org/licenses/mit-license.php
+ *
+ * @package SutraSite
+ * @link http://www.example.com/
+ *
+ * @version 1.0
+ */
+require './global.php';
+sCore::main();
