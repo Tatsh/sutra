64720859e2de8ddd570439dc4fcc8d585f4d7919
Tatsh <ddrtist@gmail.com>
2012-01-20 20:24:24 -0600

    Remove obsolete comment

--- site/index.php
+++ site/index.php
@@ -2,9 +2,6 @@
 /**
  * Initialisation.
  *
- * If not in production mode, sCore::debugCallback() is registered to
- *   be used by fCore::debug().
- *
  * @copyright Copyright (c) 2011 Poluza.
  * @author Andrew Udvare [au] <andrew@poluza.com>
  * @license http://www.opensource.org/licenses/mit-license.php
