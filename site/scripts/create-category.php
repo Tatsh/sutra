de60732bdd34e6d4778e62d606ac8847e6b2689a
Tatsh <ddrtist@gmail.com>
2012-01-22 13:54:43 -0600

    Change into a general shell script (must be run within site root)

--- site/scripts/create-category.php
+++ site/scripts/create-category.php
@@ -1,21 +1,66 @@
 #!/usr/bin/php
 <?php
 /**
+ * This script creates new categories. It must be executed from within a site
+ *   root.
+ *
+ * Example usage in Bash and similar:
+ *   ./sutra-create-category.php "My New Category"
+ *
+ * Exapmle usage in Windows command line (cmd):
+ *   php sutra-create-category.php "My New Category"
+ *
+ * @copyright Copyright (c) 2012 Poluza.
+ * @author Andrew Udvare [au] <andrew@poluza.com>
+ * @license http://www.opensource.org/licenses/mit-license.php
+ *
  * @package SutraScripts
+ * @link http://www.example.com/
+ *
+ * @version 1.0
  */
-if ($argc != 2) {
-  printf('Usage: %s CategoryName'."\n", $argv[0]);
-  exit;
+try {
+  $php_is_first = substr(strtolower($argv[0]), 0, 3) === 'php';
+
+  if ($php_is_first && $argc != 3) {
+    throw new Exception(sprintf('USAGE: php %s CATEGORY_NAME', $argv[1]));
+  }
+  else if ($argc != 2) {
+    throw new Exception(sprintf('USAGE: %s CATEGORY_NAME', $argv[0]));
+  }
+
+  $category_name = $argv[1];
+
+  // Find the site root
+  while (!is_file('./global.php')) {
+    if (!chdir('..')) {
+      throw new Exception('Cannot find site root.');
+    }
+  }
+
+  require './global.php';
+
+  if (!class_exists('fCore') || !class_exists('sDatabase') || !class_exists('Category')) {
+    throw new Exception('Site root contains global.php but is invalid.');
+  }
+
+  fCore::enableDebugging(TRUE);
+  sDatabase::getInstance();
+
+  $category = new Category;
+  $category->setName($category_name);
+  $category->setDescription($category_name);
+  $category->store();
 }
-
-$category_name = $argv[1];
-
-chdir('..');
-require './global.php';
-fCore::enableDebugging(TRUE);
-sDatabase::getInstance();
-
-$category = new Category;
-$category->setName($category_name);
-$category->setDescription($category_name);
-$category->store();
+catch (fValidationException $e) {
+  print 'Caught fValidationException: '.$e->getMessage()."\n";
+  exit(1);
+}
+catch (fEnvironmentException $e) {
+  print 'Caught fEnvironmentException: '.$e->getMessage()."\n";
+  exit(1);
+}
+catch (Exception $e) {
+  print $e->getMessage()."\n";
+  exit(1);
+}
