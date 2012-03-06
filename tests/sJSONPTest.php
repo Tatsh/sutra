bdf87c96db33dc7382eaa3e84363a347f72d0256
Tatsh <ddrtist@gmail.com>
2012-03-06 02:13:43 -0600

    Add sJSONP unit test

---
+++ tests/sJSONPTest.php
@@ -0,0 +1,30 @@
+<?php
+require './autoload.inc';
+
+class sJSONPTest extends PHPUnit_Framework_TestCase {
+  /**
+   * @expectedException fValidationException
+   */
+  public function testEncodeBadCallbackReservedWord() {
+    sJSONP::encode(array('data' => 'a'), 'super');
+  }
+
+  public function testEncode() {
+    $code = sJSONP::encode(array('data' => 'a'), 'goodCallback');
+    $this->assertEquals('goodCallback({"data":"a"});', $code);
+
+    $code = sJSONP::encode(1, 'goodCallback');
+    $this->assertEquals('goodCallback(1);', $code);
+
+    $code = sJSONP::encode(2.1, 'goodCallback');
+    $this->assertEquals('goodCallback(2.1);', $code);
+
+    $code = sJSONP::encode(true, 'goodCallback');
+    $this->assertEquals('goodCallback(true);', $code);
+
+    $b = new stdClass;
+    $b->data = 'a';
+    $code = sJSONP::encode($b, 'goodCallback');
+    $this->assertEquals('goodCallback({"data":"a"});', $code);
+  }
+}
