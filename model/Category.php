0f6dd9fd0d748a9422c5daaaafde2a1aa1e0a416
Tatsh <ddrtist@gmail.com>
2012-03-14 19:01:46 -0500

    Add helper method to get Categories as array

--- model/Category.php
+++ model/Category.php
@@ -16,11 +16,35 @@ class Category extends fActiveRecord {
    * Implements fActiveRecord::configure().
    *
    * @internal
-   * 
+   *
    * @return void
    */
   protected function configure() {
     fORMDate::configureDateCreatedColumn($this, 'date_created');
     fORMDate::configureDateUpdatedColumn($this, 'date_updated');
+  }
+
+  /**
+   * Get all the categories as a simple array with ID as the key.
+   *
+   * @param boolean $no_cache Do not try to retrieve from cache.
+   * @return array Array of categories.
+   */
+  public static function buildAsArray($no_cache = FALSE) {
+    $cache = sCache::getInstance();
+    $cached = $cache->get(__CLASS__.'::buildAsArray');
+
+    if (!$cached || $no_cache) {
+      $cached = array();
+      $records = fRecordSet::build(__CLASS__, array(), array('name' => 'asc'));
+
+      foreach ($records as $record) {
+        $cached[$record->getCategoryId()] = __($record->getName());
+      }
+
+      $cache->set(__CLASS__.'::buildAsArray', $cached, 7200);
+    }
+
+    return $cached;
   }
 }
