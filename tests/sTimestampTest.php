b63b3faf4b04368fd5a08b308b29ea4833f33e67
Tatsh <ddrtist@gmail.com>
2012-04-04 08:45:05 -0500

    Update sTimestamp tests

--- tests/sTimestampTest.php
+++ tests/sTimestampTest.php
@@ -3,17 +3,18 @@ require './00-global.php';
 
 class sTimestampTest extends PHPUnit_Framework_TestCase {
   public function testFormatTimezoneWithNumber() {
-    $this->assertEquals('-12:00', sTimestamp::formatTimezoneWithNumber(-12));
-    $this->assertEquals('+08:00', sTimestamp::formatTimezoneWithNumber(8));
-    $this->assertEquals('-08:00', sTimestamp::formatTimezoneWithNumber(-8));
-    $this->assertEquals('+12:00', sTimestamp::formatTimezoneWithNumber(12));
+    $this->assertEquals('-12:00', sTimestamp::formatTimezoneNumber(-12));
+    $this->assertEquals('+08:00', sTimestamp::formatTimezoneNumber(8));
+    $this->assertEquals('-08:00', sTimestamp::formatTimezoneNumber(-8));
+    $this->assertEquals('+12:00', sTimestamp::formatTimezoneNumber(12));
+    $this->assertEquals('+12.5:00', sTimestamp::formatTimezoneNumber(12.5));
   }
 
   /**
    * @expectedException fValidationException
    */
   public function testRfc3339ToUNIXNotParseable() {
-    sTimestamp::rfc3339ToUNIX('a', TRUE);
+    sTimestamp::RFC3339ToTimestamp('a', TRUE);
   }
 
   /**
@@ -22,7 +23,7 @@ class sTimestampTest extends PHPUnit_Framework_TestCase {
    */
   public function testRfc3339ToUNIXBadSecond() {
     // The regex has range [0-5][0-9] for the second and minute sections
-    sTimestamp::rfc3339ToUNIX('2002-10-02T22:00:60Z', TRUE);
+    sTimestamp::RFC3339ToTimestamp('2002-10-02T22:00:60Z', TRUE);
   }
 
   /**
@@ -30,7 +31,7 @@ class sTimestampTest extends PHPUnit_Framework_TestCase {
    * @expectedExceptionMessage The value specified could not be validated as a RFC3339 timestamp.
    */
   public function testRfc3339ToUNIXBadMinute() {
-    sTimestamp::rfc3339ToUNIX('2002-10-02T22:61:59Z', TRUE);
+    sTimestamp::RFC3339ToTimestamp('2002-10-02T22:61:59Z', TRUE);
   }
 
   /**
@@ -38,7 +39,7 @@ class sTimestampTest extends PHPUnit_Framework_TestCase {
    * @expectedExceptionMessage The date/time specified, 2002-10-2 25:0:0, does not appear to be a valid date/time
    */
   public function testRfc3339ToUNIXBadHour() {
-    sTimestamp::rfc3339ToUNIX('2002-10-02T25:00:00Z', TRUE);
+    sTimestamp::RFC3339ToTimestamp('2002-10-02T25:00:00Z', TRUE);
   }
 
   /**
@@ -46,7 +47,7 @@ class sTimestampTest extends PHPUnit_Framework_TestCase {
    * @expectedExceptionMessage The date/time specified, 2002-10-32 23:0:0, does not appear to be a valid date/time
    */
   public function testRfc3339ToUNIXBadDayNumber() {
-    sTimestamp::rfc3339ToUNIX('2002-10-32T23:00:00Z', TRUE);
+    sTimestamp::RFC3339ToTimestamp('2002-10-32T23:00:00Z', TRUE);
   }
 
   /**
@@ -54,7 +55,7 @@ class sTimestampTest extends PHPUnit_Framework_TestCase {
    * @expectedExceptionMessage The date/time specified, 2002-13-31 23:0:0, does not appear to be a valid date/time
    */
   public function testRfc3339ToUNIXBadMonthNumber() {
-    sTimestamp::rfc3339ToUNIX('2002-13-31T23:00:00Z', TRUE);
+    sTimestamp::RFC3339ToTimestamp('2002-13-31T23:00:00Z', TRUE);
   }
 
   /**
@@ -63,7 +64,7 @@ class sTimestampTest extends PHPUnit_Framework_TestCase {
    */
   public function testRfc3339ToUNIXBadYearNumberIncorrectLength() {
     // Year range is 1000-2999
-    sTimestamp::rfc3339ToUNIX('200-13-31T23:00:00Z', TRUE);
+    sTimestamp::RFC3339ToTimestamp('200-13-31T23:00:00Z', TRUE);
   }
 
   /**
@@ -72,26 +73,26 @@ class sTimestampTest extends PHPUnit_Framework_TestCase {
    */
   public function testRfc3339ToUNIXBadYearNumberTooLowCorrectLength() {
     // Year range is 1000-2999
-    sTimestamp::rfc3339ToUNIX('0999-13-31T23:00:00Z', TRUE);
+    sTimestamp::RFC3339ToTimestamp('0999-13-31T23:00:00Z', TRUE);
   }
 
   public function testRfc3339ToUNIXNoExceptionBadTimestamp() {
-    $this->assertEquals(FALSE, sTimestamp::rfc3339ToUNIX('2002-10-02T25:00:59+13:00'));
-    $this->assertEquals(FALSE, sTimestamp::rfc3339ToUNIX('2002-10-02T25:00:59Z'));
-    $this->assertEquals(FALSE, sTimestamp::rfc3339ToUNIX('2002-10-02T25:00:59.573Z'));
+    $this->assertEquals(FALSE, sTimestamp::RFC3339ToTimestamp('2002-10-02T25:00:59+13:00'));
+    $this->assertEquals(FALSE, sTimestamp::RFC3339ToTimestamp('2002-10-02T25:00:59Z'));
+    $this->assertEquals(FALSE, sTimestamp::RFC3339ToTimestamp('2002-10-02T25:00:59.573Z'));
   }
 
   public function testRfc3339ToUNIX() {
-    $result = sTimestamp::rfc3339ToUNIX('2002-10-02T22:00:59+10:00');
+    $result = sTimestamp::RFC3339ToTimestamp('2002-10-02T22:00:59+10:00');
     $this->assertInternalType('integer', $result);
 
-    $result = sTimestamp::rfc3339ToUNIX('2002-10-02T22:00:59.573+10:00');
+    $result = sTimestamp::RFC3339ToTimestamp('2002-10-02T22:00:59.573+10:00');
     $this->assertInternalType('integer', $result);
 
-    $result = sTimestamp::rfc3339ToUNIX('2002-10-02T22:00:59Z');
+    $result = sTimestamp::RFC3339ToTimestamp('2002-10-02T22:00:59Z');
     $this->assertInternalType('integer', $result);
 
-    $result = sTimestamp::rfc3339ToUNIX('2002-10-02T22:00:59.573Z');
+    $result = sTimestamp::RFC3339ToTimestamp('2002-10-02T22:00:59.573Z');
     $this->assertInternalType('integer', $result);
   }
 }
