07c506d44118d9f137e7beef74298c183fab65af
Tatsh <ddrtist@gmail.com>
2012-04-05 16:28:33 -0500

    Now a simple front-end to validate POST requests and call callbacks or redirect

--- web-db-dependent/sRequest.php
+++ web-db-dependent/sRequest.php
@@ -7,9 +7,9 @@
  * @license http://www.opensource.org/licenses/mit-license.php
  *
  * @package Sutra
- * @link http://www.example.com/
+ * @link http://www.sutralib.com/
  *
- * @version 1.0
+ * @version 1.2
  */
 class sRequest extends fRequest {
   /**
@@ -19,15 +19,6 @@ class sRequest extends fRequest {
    * @var string
    */
   const LAST_POST_SESSION_KEY = 'sPostRequest::last_post';
-
-  /**
-   * Path prefixes that do not need CSRF checking.
-   *
-   * @var array
-   */
-  private static $no_csrf_path_prefixes = array(
-    '/json', // may disappear later
-  );
 
   /**
    * Registered callbacks.
@@ -40,13 +31,42 @@ class sRequest extends fRequest {
   );
 
   /**
-   * Add a path prefix that does not need a CSRF check.
+   * Validate a request and redirect to a URL. All POST values are saved
+   *   when a validation exception occurs.
    *
-   * @param string $path Path prefix with leading / and optional ending /.
+   * @param fValidation $validation fValidation instance to try to validate.
+   * @param string|callback $redirect_to If not specified, will redirect to
+   *   the same URL. Also can specify a callback to call on success.
+   * @param string $error_redirect Where to redirect if an error occurs. Also
+   *   can specify a callback to call on error.
    * @return void
+   * @see sRequest::restoreLastPOSTValues()
+   * @see sRequest::deleteLastPOSTValues()
    */
-  public static function addNoCSRFPrefix($path) {
-    self::$no_csrf_path_prefixes[] = $path;
+  public static function validatePOST(fValidation $validation, $redirect_to = NULL, $error_redirect = NULL) {
+    try {
+      $url = fURL::get();
+
+      self::callCallbacks('before');
+      $validation->validate();
+      self::callCallbacks('after');
+
+      if (is_callable(fCore::callback($redirect_to))) {
+        $redirect_to();
+      }
+
+      fURL::redirect($redirect_to ? $redirect_to : $url);
+    }
+    catch (fValidationException $e) {
+      self::savePOSTValues();
+
+      if (is_callable(fCore::callback($error_redirect))) {
+        $error_redirect($e);
+        return;
+      }
+
+      fURL::redirect($error_redirect ? $error_redirect : $url);
+    }
   }
 
   /**
@@ -92,136 +112,6 @@ class sRequest extends fRequest {
   }
 
   /**
-   * Handles AJAX requests.
-   *
-   * @return void
-   * @SuppressWarnings(PHPMD.ExitExpression)
-   */
-  protected static function ajaxHandle() {
-    try {
-      fJSON::sendHeader();
-      self::callCallbacks('before');
-      self::handleCommon();
-      self::callCallbacks('after');
-      // If any data needed to be sent it should've been printed in the callback
-    }
-    catch (fValidationException $e) {
-      $ret = array(
-        'error' => self::getErrorMessageFromException($e),
-        'csrf' => fRequest::generateCSRFToken(fURL::get()),
-      );
-      print fJSON::encode($ret);
-    }
-    exit;
-  }
-
-  /**
-   * Checks if the path in use requires a CSRF.
-   *
-   * @param string $url URL to check.
-   * @return boolean If the path requires a CSRF (default TRUE).
-   */
-  protected static function requiresCSRF($url = NULL) {
-    if (!$url) {
-      $url = fURL::get();
-    }
-
-    foreach (self::$no_csrf_path_prefixes as $prefix) {
-      if (substr($url, 0, strlen($prefix)) === $prefix) {
-        return FALSE;
-      }
-    }
-
-    return TRUE;
-  }
-
-  /**
-   * Runs Moor's router.
-   *
-   * @return void
-   */
-  private static function useMoor() {
-    Moor::run();
-  }
-
-  /**
-   * The common POST handling processing code.
-   *
-   * @return void
-   */
-  private static function handleCommon() {
-    fRequest::validateCSRFToken(fRequest::get('csrf', 'string'));
-    self::useMoor();
-    self::deleteLastPOSTValues();
-  }
-
-  /**
-   * Gets the error message from the exception with tags and new lines
-   *   stripped, handling special cases as well.
-   *
-   * @param fValidationException $e Exception to use.
-   * @return string
-   */
-  private static function getErrorMessageFromException($e) {
-    $error = __(strip_tags($e->getMessage()));
-    $error = preg_replace("#(\n)+#", ' ', $error);
-
-    if (strpos($error, 'The form submitted could not be validated as authentic') !== FALSE) {
-      $error = __('An unknown error occurred. Please try again.');
-    }
-
-    return $error;
-  }
-
-  /**
-   * Called from sCore::main(). If this is not a POST request, this function
-   *   will return immediately. Otherwise, POST data will be processed and the
-   *   browser will be redirected.
-   *
-   * On error, POST values are stored in the cache key
-   *   'sPostRequest::last_post'.
-   *
-   * If the URL is exempt from CSRF validation, then this class will simply
-   *   run Moor::run() and exit.
-   *
-   * @return void
-   * @see sPostRequest::callProcessorClasses()
-   * @SuppressWarnings(PHPMD.ExitExpression)
-   */
-  public static function handle() {
-    try {
-      if (!fRequest::isPost()) {
-        return;
-      }
-
-      if (fRequest::isAjax()) {
-        self::ajaxHandle();
-      }
-
-      $url = fURL::get();
-
-      self::callCallbacks('before');
-
-      if (!self::requiresCSRF($url)) {
-        self::useMoor();
-        exit;
-      }
-
-      self::handleCommon();
-      self::callCallbacks('after');
-
-      $destination = fRequest::get('destination', 'string', '/');
-      $destination = fAuthorization::getRequestedURL(TRUE, $destination);
-      fURL::redirect($destination);
-    }
-    catch (fValidationException $e) {
-      self::savePOSTValues();
-      sMessaging::addError(self::getErrorMessageFromException($e), self::getDestinationURL());
-      fURL::redirect($destination);
-    }
-  }
-
-  /**
    * Saves the POST values to sessions.
    *
    * @return void
@@ -233,31 +123,6 @@ class sRequest extends fRequest {
       $safe_post[$key] = fRequest::get($key);
     }
     fSession::set(self::LAST_POST_SESSION_KEY, $safe_post);
-  }
-
-  /**
-   * Gets the destination URL.
-   *
-   * @return string The destination URL.
-   */
-  private static function getDestinationURL() {
-    // Redirect back to same page without /post
-    $url = explode('/', fURL::get());
-    $destination = array();
-
-    $count = sizeof($url) - 1;
-    $i = 0;
-    foreach ($url as $part) {
-      if ($part === 'post' && $i == $count) {
-        break;
-      }
-
-      $destination[] = $part;
-
-      $i++;
-    }
-
-    return implode('/', $destination);
   }
 
   /**
