de2e89c994327048518954cd7eee075b957b5b7b
Tatsh <ddrtist@gmail.com>
2012-04-05 15:36:10 -0500

    Be a bit more like fLoader

--- classes/sLoader.php
+++ classes/sLoader.php
@@ -43,13 +43,43 @@ class sLoader extends fLoader {
   );
 
   /**
-   * Override best() method. Alias for sLoader::eagar().
+   * Override best() method.
    *
    * @return void
    * @see sLoader::eagar()
    */
   public static function best() {
-    self::eagar();
+    if (self::hasOpcodeCache()) {
+      return sLoader::eagar();
+    }
+
+    self::lazy();
+  }
+
+  /**
+   * Creates constructor functions. This makes it possible to write:
+   *
+   * <code>new sProcess('node myscript')->execute();</code>
+   *
+   * @return void
+   */
+  private static function createConstructorFunctions() {
+    if (function_exists('sImage')) {
+      return;
+    }
+
+    function sImage($file_path, $skip_checks = FALSE) {
+      return new sImage($file_path, $skip_checks);
+    }
+
+    // Limited signature support
+    function sProcess($name) {
+      return new sProcess($name);
+    }
+
+    function sTimestamp($datetime, $timezone = NULL) {
+      return new sTimestamp($date, $timezone);
+    }
   }
 
   /**
@@ -59,7 +89,10 @@ class sLoader extends fLoader {
    */
   public static function eagar() {
     parent::eager();
+
     self::setPath();
+    self::createConstructorFunctions();
+
     foreach (self::$classes as $class) {
       require self::$path.$class.'.php';
     }
@@ -74,5 +107,39 @@ class sLoader extends fLoader {
     if (!self::$path) {
       self::$path = realpath(dirname(__FILE__)).'/';
     }
+  }
+
+  /**
+   * Registers a class auto-loader to load Sutra classes.
+   *
+   * @return void
+   */
+  public static function lazy() {
+    parent::lazy();
+
+    self::setPath();
+    self::createConstructorFunctions();
+
+    spl_autoload_register(array('sLoader', 'autoload'));
+  }
+
+  /**
+   * Tries to load a Sutra class.
+   *
+   * @internal
+   *
+   * @param  string $class The class to load.
+   * @return void
+   */
+  public static function autoload($class) {
+    if ($class[0] != 's' || ord($class[1]) < 65 || ord($class[1]) > 90) {
+      return;
+    }
+
+    if (!in_array($class, self::$classes)) {
+      return;
+    }
+
+    require self::$path.$class.'.php';
   }
 }
