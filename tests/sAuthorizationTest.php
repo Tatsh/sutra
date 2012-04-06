53149b3718b828bcd83b3901920cf978f34e2b3e
Tatsh <ddrtist@gmail.com>
2012-04-06 16:39:19 -0500

    Add sAuthorization test

---
+++ tests/sAuthorizationTest.php
@@ -0,0 +1,26 @@
+<?php
+require './00-global.php';
+
+class sAuthorizationTest extends PHPUnit_Framework_TestCase {
+  public function setUp() {
+    sAuthorization::setAuthLevels(array('admin' => 100, 'user' => 50));
+  }
+
+  public function testRequireNotLoggedInLoggedIn() {
+    $this->expectOutputString('http://'.$_SERVER['SERVER_NAME'].'/');
+
+    sAuthorization::setUserAuthLevel('admin');
+    sAuthorization::requireNotLoggedIn();
+  }
+
+  public function testRequireNotLoggedIn() {
+    $this->assertNull(sAuthorization::requireNotLoggedIn());
+  }
+
+  public function testRequireNotLoggedInURLArgument() {
+    $this->expectOutputString('http://'.$_SERVER['SERVER_NAME'].'/404');
+
+    sAuthorization::setUserAuthLevel('admin');
+    sAuthorization::requireNotLoggedIn('/404');
+  }
+}
