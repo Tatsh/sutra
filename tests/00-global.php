a0c9321a646251d60a3ac675c4b230821641ac43
Tatsh <ddrtist@gmail.com>
2012-04-06 14:52:46 -0500

    Set some  variables for testing

--- tests/00-global.php
+++ tests/00-global.php
@@ -1,6 +1,12 @@
 <?php
 // @codeCoverageIgnoreStart
+ob_start();
 require './autoload.inc';
 require './block-exit.inc';
 require './stubs.inc';
+
+$_SERVER['SERVER_NAME'] = 'example.com';
+$_SERVER['REQUEST_URI'] = '/index.php';
+$_SERVER['REQUEST_METHOD'] = 'GET';
+$_SERVER['SERVER_PORT'] = 80;
 // @codeCoverageIgnoreEnd
