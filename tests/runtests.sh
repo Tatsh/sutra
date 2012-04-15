3aa596201f2db58eb867d7fe6ed228fdc45c2a2a
Tatsh <ddrtist@gmail.com>
2012-04-14 23:07:07 -0500

    96% code coverage with sTemplate test

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 TESTS="${1:-.}"
 mkdir -p template/default
+mkdir -p template/custom
+mkdir mincss
+
+echo '<?php print $abc; ?>' > template/default/something.tpl.php
+echo '<?php print $abc; ?>' > template/custom/something2.tpl.php
+echo '<!DOCTYPE html>
+<html lang="<?php print $lang; ?>" dir="<?php print $dir; ?>">
+<head>
+  <title><?php print $title; ?> | <?php print $site_name; ?></title>
+  <?php print $css; ?>
+  <?php print $head_js; ?>
+</head>
+<body class="<?php print $body_class; ?>">
+<div id="site-slogan">
+<?php print $site_slogan; ?>
+</div>
+<?php print $content; ?>
+<?php print $body_js; ?>
+</body>
+</html>' > template/custom/page.tpl.php
+echo '* {margin:0;padding:0}' > test.css
+echo '* {margin:0;padding:0}' > test2.css
+echo '* {margin:0;padding:0}' > test3.css
 
 cp -f resources/backup/* resources
 rm -f resources/*_copy*
@@ -13,7 +36,7 @@ phpunit --stderr -c config.xml "$TESTS"
 chmod 0700 non-writable-directory &> /dev/null
 rmdir non-writable-directory &> /dev/null
 
-rm -rf flourish__* template
+rm -rf flourish__* template mincss test*.css
 rm -f resources/*_copy*
 
 echo
--- tests/sTemplateTest.php
+++ tests/sTemplateTest.php
@@ -144,6 +144,9 @@ class sTemplateTest extends PHPUnit_Framework_TestCase {
     $this->assertEquals(array(), sTemplate::getCDNs());
   }
 
+  /**
+   * @covers sTemplate::removeCDN
+   */
   public function testRemoveCDN() {
     sTemplate::addCDN('http://myhost');
     sTemplate::addCDN('http://myhost2');
@@ -179,5 +182,149 @@ class sTemplateTest extends PHPUnit_Framework_TestCase {
    */
   public function testRenderNoPageTemplate() {
     sTemplate::render(array('title' => 'a', 'content' => 'a'));
+  }
+
+  public function testSetSiteSlogan() {
+    sTemplate::setMode('development');
+    sTemplate::setSiteSlogan('Site slogan');
+    sTemplate::setActiveTemplate('custom');
+    sTemplate::render(array('title' => 'My title', 'content' => 'a'));
+    $this->expectOutputRegex('/>\\nSite\sslogan\\n<\/div>/');
+
+    sTemplate::setMode('production');
+    sTemplate::render(array('title' => 'My title', 'content' => 'a'));
+    $this->expectOutputRegex('/>Site\sslogan<\/div>/');
+  }
+
+  public function testSetSiteName() {
+    sTemplate::setMode('development');
+    sTemplate::setSiteName('my site');
+    sTemplate::setActiveTemplate('custom');
+    sTemplate::render(array('title' => 'My title', 'content' => 'a'));
+    $this->expectOutputRegex('/<title>My\stitle\s\|\smy\ssite<\/title>/');
+  }
+
+  public function testEnableQueryStrings() {
+    sTemplate::setMode('development');
+    sTemplate::enableQueryStrings(TRUE); // default
+    sTemplate::setActiveTemplate('custom');
+    sTemplate::render(array('title' => 'Test query string enabled', 'content' => ''));
+    $this->expectOutputRegex('/<script\ssrc="\/a\.js\?_=\d+/');
+  }
+
+  public function testEnableQueryStringsProductionModeOn() {
+    sTemplate::setMode('production');
+    sTemplate::enableQueryStrings(TRUE); // default
+    sTemplate::render(array('title' => 'Test query string enabled, production mode on', 'content' => ''));
+    $this->expectOutputRegex('/<script\ssrc="\/a\.min\.js"/');
+  }
+
+  public function testEnableQueryStringsProductionModeOff() {
+    sTemplate::setMode('development');
+    sTemplate::enableQueryStrings(FALSE); // default
+    sTemplate::render(array('title' => 'Test query string disabled, production mode off', 'content' => ''));
+    $this->expectOutputRegex('/<script\ssrc="\/a\.js"/');
+  }
+
+  public function testAddJavaScriptFileURL() {
+    sTemplate::setMode('development');
+    sTemplate::addJavaScriptFile('https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', 'head');
+    sTemplate::render(array('title' => 'Test query string disabled, production mode off', 'content' => ''));
+    $this->expectOutputRegex('/<head>\n\s+[^>]+.*<\/title>\n.*<script\ssrc="https/');
+  }
+
+  public function testAddCSSFile() {
+    sTemplate::setMode('development');
+    sTemplate::addCSSFile('test.css');
+    sTemplate::addCSSFile('test2.css', 'screen');
+    sTemplate::addCSSFile('test3.css', 'only screen and (min-device-width:1024px)', TRUE);
+    sTemplate::render(array('title' => 'Test CSS, production mode off', 'content' => ''));
+    $this->expectOutputRegex('/<link\shref="\/test\.css"\smedia="all"/');
+    $this->expectOutputRegex('/<link\shref="\/test2\.css"\smedia="screen"/');
+    $this->expectOutputRegex('/<link\shref="\/test3\.css"\smedia="only\sscreen\sand\s\(min/');
+  }
+
+  /**
+   * @depends testAddCSSFile
+   */
+  public function testSetMediaOrder() {
+    sTemplate::setMode('development');
+    sTemplate::setCSSMediaOrder(array('screen', 'all'));
+    sTemplate::render(array('title' => 'Test CSS', 'content' => ''));
+    $this->expectOutputRegex('/<link\shref="\/test2\.css[^>]+>[^<]+<link href="\/test\.css"/');
+  }
+
+  public static function theCallback1() {
+    return array('abc' => 'fghijk');
+  }
+
+  public static function theCallback2() {
+    return array('abc' => 'abcdef');
+  }
+
+  public static function badCallback() {
+    return NULL;
+  }
+
+  public function testRegisterCallback() {
+    sTemplate::registerCallback(__CLASS__.'::theCallback1', 'something');
+    sTemplate::registerCallback(__CLASS__.'::theCallback2', 'something2');
+
+    $content = sTemplate::buffer('something');
+    $this->assertEquals('fghijk', $content);
+
+    $content = sTemplate::buffer('something2');
+    $this->assertEquals('abcdef', $content);
+  }
+
+  /**
+   * @expectedException fProgrammerException
+   * @expectedExceptionMessage Callback "sTemplateTest::badCallback" for template "something" did not return an array.
+   */
+  public function testRegisterBadCallback() {
+    sTemplate::registerCallback(__CLASS__.'::badCallback', 'something');
+    $content = sTemplate::buffer('something');
+  }
+
+  /**
+   * @expectedException fProgrammerException
+   */
+  public function testSetActiveTemplateBadFallbackTemplate() {
+    sTemplate::setActiveTemplate('./resources', 'non-existant');
+  }
+
+  public function testSetCDNs() {
+    $cdns = array('http://myhost', 'http://myhost2');
+    sTemplate::setCDNs($cdns);
+    sTemplate::setMode('development');
+    $this->assertEquals('', sTemplate::getACDN());
+
+    sTemplate::setMode('production');
+    $this->assertTrue(in_array(sTemplate::getACDN(), $cdns, TRUE));
+    sTemplate::setMode('development');
+  }
+
+  /**
+   * @depends testAddCSSFile
+   */
+  public function testRender() {
+    sTemplate::setMode('production');
+    sTemplate::setMinifiedCSSPath('mincss');
+    sTemplate::setProductionModeTemplatesPath('template');
+    sTemplate::setActiveTemplate('custom');
+    sTemplate::render(array('title' => 'General render', 'content' => 'a'));
+    $this->expectOutputRegex('/mincss\/css\-only\-screen\-and\-min\-device\-width\-1024px\-\d+/');
+  }
+
+  /**
+   * @depends testRender
+   * @expectedException fUnexpectedException
+   * @expectedExceptionMessage Unable to read file "non-existant.css"
+   */
+  public function testRenderBadCSSFile() {
+    sTemplate::getCache()->clear();
+
+    sTemplate::addCSSFile('non-existant.css');
+    sTemplate::render(array('title' => 'General render', 'content' => 'a'));
   }
 }
