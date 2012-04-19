6e0a3cc176373feefc62d3139b2ed7caac3c47dd
Tatsh <ddrtist@gmail.com>
2012-04-19 15:21:53 -0500

    Add Kate modeline

--- qa.sh
+++ qa.sh
@@ -3,8 +3,10 @@ CLASS="${1:-.}"
 pushd classes
 
 if [[ $CLASS != '.' && "${CLASS:(-4)}" != ".php" ]]; then
-  CLASS="$CLASS.php"
+	CLASS="$CLASS.php"
 fi
 
 phpmd "$CLASS" text ../phpmd-ruleset.xml
 popd
+
+# kate: replace-tabs false;
