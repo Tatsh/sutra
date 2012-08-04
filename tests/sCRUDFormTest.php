fb218b564e4cb8ffed6eb0a45783b9438d154e4e
Tatsh <audvare@gmail.com>
2012-08-04 01:22:11 -0700

    Build database for this test

--- tests/sCRUDFormTest.php
+++ tests/sCRUDFormTest.php
@@ -1,13 +1,30 @@
 <?php
 require './includes/global.inc';
 
+class CrudTestTable extends fActiveRecord {}
+
 class sCRUDFormTest extends PHPUnit_Framework_TestCase {
   /**
-   * @expectedException fProgrammerException
-   * @expectedExceptionMessage Action URL is invalid. Must be at least 1 character long
+   * @var fDatabase
    */
-  public function testConstructorBadAction() {
-    new sCRUDForm('a', '');
+  private static $db = NULL;
+
+  public static function setUpBeforeClass() {
+    $sql =<<<SQL
+    DROP TABLE IF EXISTS crud_test_tables;
+    CREATE TABLE crud_test_tables (
+      tid INTEGER AUTOINCREMENT PRIMARY KEY,
+      column_a TEXT NOT NULL
+    );
+SQL;
+
+    self::$db = new fDatabase('sqlite', './resources/db.sqlite3');
+    self::$db->translatedExecute($sql);
+    fORMDatabase::attach(self::$db);
+  }
+
+  public static function tearDownAfterClass() {
+    self::$db->translatedExecute('DROP TABLE crud_test_tables');
   }
 
   /**
@@ -15,6 +32,6 @@ class sCRUDFormTest extends PHPUnit_Framework_TestCase {
    * @expectedExceptionMessage Request method "delete" is invalid. Must be one of: get,post
    */
   public function testConstructorBadMethod() {
-    new sCRUDForm('a', '/', 'delete');
+    new sCRUDForm('CrudTestTable', '/', 'delete');
   }
 }
