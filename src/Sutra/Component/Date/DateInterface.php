29be1abae7355f13d5e5f1dd1bc17faf46d941d7
Andrew Udvare <audvare@gmail.com>
2013-09-23 21:38:43 -0700

    DateInterface as interface for replacement of fDate

---
+++ src/Sutra/Component/Date/DateInterface.php
@@ -0,0 +1,53 @@
+<?php
+namespace Sutra\Component\Date;
+
+/**
+ * @replaces fDate
+ */
+interface DateInterface
+{
+    /**
+     * @replaces ::adjust
+     */
+    public function adjust($adjustment);
+
+    /**
+     * @replaces ::eq
+     */
+    public function equals($otherDate = null);
+
+    /**
+     * @replaces ::format
+     */
+    public function format($format);
+
+    /**
+     * @replaces ::getFuzzyDifference
+     */
+    public function getFuzzyDifference($otherDate = null, $simple = false);
+
+    /**
+     * @replaces ::gt
+     */
+    public function greaterThan($otherDate = null);
+
+    /**
+     * @replaces ::gte
+     */
+    public function greaterThanOrEqualTo($otherDate = null);
+
+    /**
+     * @replaces ::lt
+     */
+    public function lessThan($otherDate = null);
+
+    /**
+     * @replaces ::lte
+     */
+    public function lessThanOrEqualTo($otherDate = null);
+
+    /**
+     * @replaces ::modify
+     */
+    public function modify($format);
+}
