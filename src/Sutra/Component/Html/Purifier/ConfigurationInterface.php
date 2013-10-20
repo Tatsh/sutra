5069089fffae8732a0e087399f2301af09a7805c
Andrew Udvare <audvare@gmail.com>
2013-10-20 01:48:58 -0700

    Add TODO

--- src/Sutra/Component/Html/Purifier/ConfigurationInterface.php
+++ src/Sutra/Component/Html/Purifier/ConfigurationInterface.php
@@ -4,6 +4,8 @@ namespace Sutra\Component\Html\Purifier;
 /**
  * Interface for HTML Purifier configuration for type checking but without
  *   real class.
+ *
+ * @todo Put methods here (set, get, etc).
  */
 interface ConfigurationInterface
 {
